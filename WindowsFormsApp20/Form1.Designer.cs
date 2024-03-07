8b8903d0c579fbf134c9dadc247d56bab4038770
MrBartlett <101218295+MrBartlett@users.noreply.github.com>
2024-03-07 13:30:35 +1100

    InitialPush

---
+++ WindowsFormsApp20/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace WindowsFormsApp20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerRight = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pb33 = new System.Windows.Forms.PictureBox();
+            this.pb23 = new System.Windows.Forms.PictureBox();
+            this.pb13 = new System.Windows.Forms.PictureBox();
+            this.pb03 = new System.Windows.Forms.PictureBox();
+            this.pb32 = new System.Windows.Forms.PictureBox();
+            this.pb22 = new System.Windows.Forms.PictureBox();
+            this.pb12 = new System.Windows.Forms.PictureBox();
+            this.pb02 = new System.Windows.Forms.PictureBox();
+            this.pb31 = new System.Windows.Forms.PictureBox();
+            this.pb21 = new System.Windows.Forms.PictureBox();
+            this.pb11 = new System.Windows.Forms.PictureBox();
+            this.pb01 = new System.Windows.Forms.PictureBox();
+            this.pb30 = new System.Windows.Forms.PictureBox();
+            this.pb20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pb00 = new System.Windows.Forms.PictureBox();
+            this.pb10 = new System.Windows.Forms.PictureBox();
+            this.timerLeft = new System.Windows.Forms.Timer(this.components);
+            this.timerDown = new System.Windows.Forms.Timer(this.components);
+            this.timerUp = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb33)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb32)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb31)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb30)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerRight
+            // 
+            this.timerRight.Tick += new System.EventHandler(this.timerright_Tick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.Controls.Add(this.pb33, 4, 4);
+            this.tableLayoutPanel1.Controls.Add(this.pb23, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.pb13, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.pb03, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.pb32, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pb22, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pb12, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pb02, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pb31, 4, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pb21, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pb11, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pb01, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pb30, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pb20, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pb00, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pb10, 2, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 10);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(681, 407);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pb33
+            // 
+            this.pb33.BackColor = System.Drawing.Color.White;
+            this.pb33.Location = new System.Drawing.Point(507, 303);
+            this.pb33.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb33.Name = "pb33";
+            this.pb33.Size = new System.Drawing.Size(160, 94);
+            this.pb33.TabIndex = 3;
+            this.pb33.TabStop = false;
+            // 
+            // pb23
+            // 
+            this.pb23.BackColor = System.Drawing.Color.White;
+            this.pb23.Location = new System.Drawing.Point(341, 303);
+            this.pb23.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb23.Name = "pb23";
+            this.pb23.Size = new System.Drawing.Size(160, 94);
+            this.pb23.TabIndex = 3;
+            this.pb23.TabStop = false;
+            // 
+            // pb13
+            // 
+            this.pb13.BackColor = System.Drawing.Color.White;
+            this.pb13.Location = new System.Drawing.Point(175, 303);
+            this.pb13.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb13.Name = "pb13";
+            this.pb13.Size = new System.Drawing.Size(160, 94);
+            this.pb13.TabIndex = 3;
+            this.pb13.TabStop = false;
+            // 
+            // pb03
+            // 
+            this.pb03.BackColor = System.Drawing.Color.White;
+            this.pb03.Location = new System.Drawing.Point(9, 303);
+            this.pb03.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb03.Name = "pb03";
+            this.pb03.Size = new System.Drawing.Size(160, 94);
+            this.pb03.TabIndex = 3;
+            this.pb03.TabStop = false;
+            // 
+            // pb32
+            // 
+            this.pb32.BackColor = System.Drawing.Color.White;
+            this.pb32.Location = new System.Drawing.Point(507, 204);
+            this.pb32.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb32.Name = "pb32";
+            this.pb32.Size = new System.Drawing.Size(160, 91);
+            this.pb32.TabIndex = 3;
+            this.pb32.TabStop = false;
+            // 
+            // pb22
+            // 
+            this.pb22.BackColor = System.Drawing.Color.White;
+            this.pb22.Location = new System.Drawing.Point(341, 204);
+            this.pb22.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb22.Name = "pb22";
+            this.pb22.Size = new System.Drawing.Size(160, 94);
+            this.pb22.TabIndex = 3;
+            this.pb22.TabStop = false;
+            // 
+            // pb12
+            // 
+            this.pb12.BackColor = System.Drawing.Color.White;
+            this.pb12.Location = new System.Drawing.Point(175, 204);
+            this.pb12.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb12.Name = "pb12";
+            this.pb12.Size = new System.Drawing.Size(160, 94);
+            this.pb12.TabIndex = 3;
+            this.pb12.TabStop = false;
+            // 
+            // pb02
+            // 
+            this.pb02.BackColor = System.Drawing.Color.White;
+            this.pb02.Location = new System.Drawing.Point(9, 204);
+            this.pb02.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb02.Name = "pb02";
+            this.pb02.Size = new System.Drawing.Size(160, 94);
+            this.pb02.TabIndex = 3;
+            this.pb02.TabStop = false;
+            // 
+            // pb31
+            // 
+            this.pb31.BackColor = System.Drawing.Color.White;
+            this.pb31.Location = new System.Drawing.Point(507, 105);
+            this.pb31.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb31.Name = "pb31";
+            this.pb31.Size = new System.Drawing.Size(160, 94);
+            this.pb31.TabIndex = 3;
+            this.pb31.TabStop = false;
+            // 
+            // pb21
+            // 
+            this.pb21.BackColor = System.Drawing.Color.White;
+            this.pb21.Location = new System.Drawing.Point(341, 105);
+            this.pb21.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb21.Name = "pb21";
+            this.pb21.Size = new System.Drawing.Size(160, 94);
+            this.pb21.TabIndex = 3;
+            this.pb21.TabStop = false;
+            // 
+            // pb11
+            // 
+            this.pb11.BackColor = System.Drawing.Color.White;
+            this.pb11.Location = new System.Drawing.Point(175, 105);
+            this.pb11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb11.Name = "pb11";
+            this.pb11.Size = new System.Drawing.Size(160, 94);
+            this.pb11.TabIndex = 3;
+            this.pb11.TabStop = false;
+            // 
+            // pb01
+            // 
+            this.pb01.BackColor = System.Drawing.Color.White;
+            this.pb01.Location = new System.Drawing.Point(9, 105);
+            this.pb01.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb01.Name = "pb01";
+            this.pb01.Size = new System.Drawing.Size(160, 94);
+            this.pb01.TabIndex = 3;
+            this.pb01.TabStop = false;
+            // 
+            // pb30
+            // 
+            this.pb30.BackColor = System.Drawing.Color.White;
+            this.pb30.Location = new System.Drawing.Point(507, 6);
+            this.pb30.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb30.Name = "pb30";
+            this.pb30.Size = new System.Drawing.Size(160, 94);
+            this.pb30.TabIndex = 3;
+            this.pb30.TabStop = false;
+            // 
+            // pb20
+            // 
+            this.pb20.BackColor = System.Drawing.Color.White;
+            this.pb20.Location = new System.Drawing.Point(341, 6);
+            this.pb20.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb20.Name = "pb20";
+            this.pb20.Size = new System.Drawing.Size(160, 94);
+            this.pb20.TabIndex = 3;
+            this.pb20.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1, 1);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pb00
+            // 
+            this.pb00.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb00.Location = new System.Drawing.Point(9, 6);
+            this.pb00.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb00.Name = "pb00";
+            this.pb00.Size = new System.Drawing.Size(160, 94);
+            this.pb00.TabIndex = 0;
+            this.pb00.TabStop = false;
+            // 
+            // pb10
+            // 
+            this.pb10.BackColor = System.Drawing.Color.White;
+            this.pb10.Location = new System.Drawing.Point(175, 6);
+            this.pb10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb10.Name = "pb10";
+            this.pb10.Size = new System.Drawing.Size(160, 94);
+            this.pb10.TabIndex = 2;
+            this.pb10.TabStop = false;
+            // 
+            // timerLeft
+            // 
+            this.timerLeft.Tick += new System.EventHandler(this.timerLeft_Tick);
+            // 
+            // timerDown
+            // 
+            this.timerDown.Tick += new System.EventHandler(this.timerDown_Tick);
+            // 
+            // timerUp
+            // 
+            this.timerUp.Tick += new System.EventHandler(this.timerUp_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(709, 84);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 125);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 438);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb33)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb32)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb31)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb30)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerRight;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pb33;
+        private System.Windows.Forms.PictureBox pb23;
+        private System.Windows.Forms.PictureBox pb13;
+        private System.Windows.Forms.PictureBox pb03;
+        private System.Windows.Forms.PictureBox pb32;
+        private System.Windows.Forms.PictureBox pb22;
+        private System.Windows.Forms.PictureBox pb12;
+        private System.Windows.Forms.PictureBox pb02;
+        private System.Windows.Forms.PictureBox pb31;
+        private System.Windows.Forms.PictureBox pb21;
+        private System.Windows.Forms.PictureBox pb11;
+        private System.Windows.Forms.PictureBox pb01;
+        private System.Windows.Forms.PictureBox pb30;
+        private System.Windows.Forms.PictureBox pb20;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pb00;
+        private System.Windows.Forms.PictureBox pb10;
+        private System.Windows.Forms.Timer timerLeft;
+        private System.Windows.Forms.Timer timerDown;
+        private System.Windows.Forms.Timer timerUp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp20/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp20
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables, which can be accessed from anywhere
+        public PictureBox[,] gamespace;
+
+        //Starting xPostition
+        public int xPos = 0;
+
+        //Starting yPosition
+        public int yPos = 0;
+
+        //public variable to set tick rate of timers
+        public int TickRate = 95;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // calls GeneratePlayArea Method when the form is first opened
+            GeneratePlayArea();
+            
+            
+
+        }
+
+
+        // Method for generating play area 2Darray
+        private void GeneratePlayArea()
+        {
+            // Declares an 2D Array 4 indexes wide, and 4 indexes tall to hold picture boxes 
+            // from grid on the form
+            PictureBox [,] pictureBoxArray = new PictureBox [4,4];
+
+            // assigns pictureboxes from Grid to array
+            pictureBoxArray[0, 0] = pb00;
+            pictureBoxArray[1, 0] = pb10;
+            pictureBoxArray[2, 0] = pb20;
+            pictureBoxArray[3, 0] = pb30;
+            pictureBoxArray[0, 1] = pb01;
+            pictureBoxArray[1, 1] = pb11;
+            pictureBoxArray[2, 1] = pb21;
+            pictureBoxArray[3, 1] = pb31;
+            pictureBoxArray[0, 2] = pb02;
+            pictureBoxArray[1, 2] = pb12;
+            pictureBoxArray[2, 2] = pb22;
+            pictureBoxArray[3, 2] = pb32;
+            pictureBoxArray[0, 3] = pb03;
+            pictureBoxArray[1, 3] = pb13;
+            pictureBoxArray[2, 3] = pb23;
+            pictureBoxArray[3, 3] = pb33;
+
+            gamespace = pictureBoxArray;
+        }
+
+        //Event triggered by pressing key
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Right arrow key press where e = key that was pressed and Keys.Right refers to right key on keyboard
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                //sets interval of timer "ticks"
+                timerRight.Interval = TickRate;
+                //Starts timerRight
+                timerRight.Start();
+
+            }
+
+            //left arrow left keypress  where e = key that was pressed and Keys.Left refers to left key on keyboard
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                //sets interval of timer "ticks"
+                timerLeft.Interval = TickRate;
+                //Starts timerLeft
+                timerLeft.Start();
+
+            }
+
+            //down arrow down keypress where e = key that was pressed and Keys.Down refers to down key on keyboard
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                //sets interval of timer "ticks"
+                timerDown.Interval = TickRate;
+                //Starts timerDown
+                timerDown.Start();
+
+            }
+
+            //left arrow Up keypress where e = key that was pressed and Keys.Up refers to up key on keyboard
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                //sets interval of timer "ticks"
+                timerUp.Interval = TickRate;
+                //Starts timerUp
+                timerUp.Start();
+
+            }
+        }
+
+        // Event triggered by releasing a key
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Right arrow key release where e = key that was pressed and Keys.Right refers to right key on keyboard
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                //stops timerright
+                timerRight.Stop();
+
+            }
+            //left arrow key release where e = key that was pressed and Keys.Left refers to left key on keyboard
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                //stops timerleft
+                timerLeft.Stop();
+
+            }
+            //down arrow key release where e = key that was pressed and Keys.Down refers to down key on keyboard
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                //stops timerdown
+                timerDown.Stop();
+
+            }
+            //up arrow key release where e = key that was pressed and Keys.Up refers to up key on keyboard
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                //stops timerdown
+                timerUp.Stop();
+
+            }
+        }
+
+        //Controls what happens every time the timerRight "ticks"
+        private void timerright_Tick(object sender, EventArgs e)
+        {
+            //Try catch to prevent errors from going outside array
+            try
+            {
+                //checks if the position + 1 (to the right of xPos) is empty (white) 
+                // If the space is empty it sets it to occupied (black) and adds 1 to the
+                // xPos position variable and sets the new square, black and the original square white.
+
+                if (gamespace[xPos + 1, yPos].BackColor == Color.White)
+                {
+                    gamespace[xPos + 1, yPos].BackColor = Color.Black;
+                    
+                    gamespace[xPos, yPos].BackColor = Color.White;
+
+                    xPos++;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        //determines what the left timer does on every tick
+        private void timerLeft_Tick(object sender, EventArgs e)
+        {
+
+            //checks if the X position - 1 (to the left of xPos) is empty (white) 
+            // If the space is empty it sets it to occupied (black) and subtracts 1 from the
+            // xPos position variable and sets the new square, black and the original square white.
+            try
+            {
+                if (gamespace[xPos - 1, yPos].BackColor == Color.White)
+                {
+                    gamespace[xPos - 1, yPos].BackColor = Color.Black;
+                    gamespace[xPos, yPos].BackColor = Color.White;
+
+                    xPos--;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        // determines what the "timerdown" does ever tick
+        private void timerDown_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //checks if the Y position + 1 (to the BELOW current yPos) is empty (white) 
+                // If the space is empty it sets it to occupied (black) and adds 1 to the
+                // yPos position variable and sets the new square, black and the original square white.
+
+                if (gamespace[xPos, yPos+1].BackColor == Color.White)
+                {
+                    gamespace[xPos, yPos+1].BackColor = Color.Black;
+                    gamespace[xPos, yPos].BackColor = Color.White;
+
+                    yPos++;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void timerUp_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //checks if the Y position - 1 (to the ABOVE current yPos) is empty (white) 
+                // If the space is empty it sets it to occupied (black) and subtracts 1 from the
+                // yPos position variable and sets the new square, black and the original square white.
+
+                if (gamespace[xPos, yPos - 1].BackColor == Color.White)
+                {
+                    gamespace[xPos, yPos - 1].BackColor = Color.Black;
+                    gamespace[xPos, yPos].BackColor = Color.White;
+
+                    yPos--;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
